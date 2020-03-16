1f5291ee40d468c2da2fdfb27d02372ed92c182a
lishal <52682738+lishal@users.noreply.github.com>
2020-03-16 08:21:24 +0545

    Database tables created for companies table

--- database/migrations/2020_03_16_021210_create_company_table.php
+++ database/migrations/2020_03_16_021210_create_company_table.php
@@ -13,8 +13,19 @@ class CreateCompanyTable extends Migration
      */
     public function up()
     {
-        Schema::create('company', function (Blueprint $table) {
-            $table->bigIncrements('id');
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_type_id');
+            $table->string('company_name', 100);
+            $table->string('company_address')->nullable();
+            $table->string('company_owner', 50)->nullable();
+            $table->string('company_phone_number', 25)->nullable();
+            $table->string('company_email', 50)->unique()->nullable();
+            $table->integer('company_user_id')->default(0);
+            $table->date('subscription_start_date')->nullable();
+            $table->date('subscription_end_date')->nullable();
+            $table->bigInteger('view_count')->default(0);
+            $table->integer('status')->nullable()->default(1);
             $table->timestamps();
         });
     }
