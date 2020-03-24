4be28114ef7cff7f15be0ad744655ddf5a0ffeb1
lishal <52682738+lishal@users.noreply.github.com>
2020-03-24 20:17:49 +0545

    yearly records table created

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -69,6 +69,6 @@ class CompanyController extends Controller
 
    
 
-    
+   
 
 }
---
+++ database/migrations/2020_03_24_143007_create_yearly_records_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateYearlyRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('yearly_records', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('yearly_records');
+    }
+}
