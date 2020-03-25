951a8810ecb12ce191a044fbc3f863a6ddc9279b
lishal <52682738+lishal@users.noreply.github.com>
2020-03-25 10:33:53 +0545

    Fiscal year created

--- app/Http/Controllers/FiscalYear.php
+++ app/Http/Controllers/FiscalYear.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
 
-class FiscalYear extends Controller
+class FiscalYear extends Model
 {
     //
 }
