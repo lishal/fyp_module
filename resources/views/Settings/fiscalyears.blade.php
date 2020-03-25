d0d4d6dbebe44facc28603c0d3829b52c24a8c57
lishal <52682738+lishal@users.noreply.github.com>
2020-03-25 11:34:35 +0545

    Adding base layout for Fiscal year

---
+++ resources/views/Settings/fiscalyears.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.mainlayout')
+@section('content-header')
+<h2>Fiscal Year </h2>
+@endsection
+@section('content')
+<div class="add-button">
+    <a href="{{ url('/#') }}" class="btn btn-primary">Add Fiscal Year</a>
+</div>
+        
+<div class="panel-body">
+    <div class="panel-top mb-5">
+        @include('validation.messages')
+    </div>
+    
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            Fiscal Year
+        </div>
+    </div>
+</div>
+
+@endsection
