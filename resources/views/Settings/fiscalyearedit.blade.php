1186894a5833886082e034f5d496ed98b7add0a4
lishal <52682738+lishal@users.noreply.github.com>
2020-03-25 13:16:18 +0545

    Text field of fiscal year

--- resources/views/Settings/fiscalyearedit.blade.php
+++ resources/views/Settings/fiscalyearedit.blade.php
@@ -13,6 +13,64 @@
                 @include('validation.messages')
                 <form class="form-horizontal" role="form" method="POST" action="{{ url('/settings/fiscalyear/save') }}" enctype="multipart/form-data">
                     @csrf
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">Name*</label>
+
+                        <div class="col-md-7">
+                            <input type="text" class="form-control" placeholder="Fiscal Year Name" name="fiscal_year_name" >
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">Start Date (in BS)*</label>
+
+                        <div class="col-md-7">
+                            <input type="text" class="form-control" placeholder="Fiscal Year Start Date (in BS)" id="nepali_year_start_date_bs" name="nepali_year_start_date_bs"  >
+
+                    
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">Start Date (in AD)*</label>
+
+                        <div class="col-md-7">
+                            <input type="text" class="form-control" placeholder="Fiscal Year Start Date (in AD)" id="englishDateStart" name="fiscal_year_start_date_ad" >
+
+                            
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">End Date (in BS)*</label>
+
+                        <div class="col-md-7">
+                            <input type="text" class="form-control" placeholder="Fiscal Year End Date (in BS)" id="nepali_year_end_date_bs" name="nepali_year_end_date_bs" >
+
+                           
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">End Date (in AD)*</label>
+
+                        <div class="col-md-7">
+                            <input type="text" class="form-control" placeholder="Fiscal Year End Date (in AD)" id="englishDateEnd" name="fiscal_year_end_date_ad" >
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">Is Current Fiscal Year?*</label>
+
+                        <div class="col-md-7">
+                            <input type="radio" name="current_fiscal_year" > Yes
+                            <input type="radio" name="current_fiscal_year" > No
+
+                        </div>
+                    </div>
+
+                    <div class="form-group bi-form-controls">
+                        <div class="col-md-9 text-left">
+                            <button type="submit" class="btn btn-default pull-right">SAVE</button>
+                        </div>
                 </form>
             </div>
         </div>
