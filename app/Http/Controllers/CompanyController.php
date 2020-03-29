14c9ae36dfa0bdd236ca77b30e6472d5af355fbf
lishal <52682738+lishal@users.noreply.github.com>
2020-03-29 20:25:52 +0545

    Changes made in company controller

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\company;
 use App\Type;
 use App\User;
+use App\YearlyRecord;
+use App\FiscalYear;
 class CompanyController extends Controller
 {
     protected $company;
@@ -72,95 +74,95 @@ class CompanyController extends Controller
 
     public function save(Request $request)
     {
-    switch ($request->input('action')) {
-        case 'save':
-            $company_id = $this->request->input('company_id');
-
-            $rules = [
-                'company_name'      => 'required|min:3|max:255',
-                'company_type'      => 'required|numeric',
-                'address'           => 'required|min:3|max:255',
-                'owner'             => 'required|min:3|max:255',
-            
-            ];
-
-
-
-            $this->validate($request, $rules);
-
-            $company_type       = $this->request->input('company_type');
-            $company_user_id    = $this->request->input('company_user_id');
-            $company_name       = $this->request->input('company_name');
-            $company_address    = $this->request->input('address');
-            $company_owner      = $this->request->input('owner');
-            $company_phone_number    = $this->request->input('phone_number');
-            // $company_vat_number      = $this->request->input('company_vat_number');
-            $subscription_start_date = $this->request->input('subscription_start_on');
-            $subscription_end_date   = $this->request->input('subscription_end_on');
-            $balance                 = $this->request->input('balance');
-
-
-            $status = $this->request->input('status');
-        
-
-            $data = [
-                    'company_type_id'   => $company_type, 
-                    'company_name'      => $company_name,
-                    'company_address'   => $company_address,
-                    'company_owner'     => $company_owner,
-                    'company_phone_number' => $company_phone_number,
-                    'company_user_id'      => $company_user_id,
-                    // 'company_vat_number'   => $company_vat_number,
-                    'subscription_start_date'   => date('Y-m-d', strtotime($subscription_start_date)),
-                    'subscription_end_date'     => date('Y-m-d', strtotime($subscription_end_date)),
-                    'status'                    => $status,
-                    'updated_at'                => date('Y-m-d H:i:s')
-                ];
-
-
-
-            if($company_id == 0) {
-                $data['created_at'] = date('Y-m-d H:i:s');
-                $company_id = \DB::table('companies')->insertGetId($data);
-                $message = "Record added successfully.";
-
-                $yearly_record = YearlyRecord::where('company_id', '=', $company_id)->get()->toArray();
-
-                if(count($yearly_record) == 0){
-                    $active_fiscal_year     = FiscalYear::where('current_fiscal_year', '1')->first();
-
-
-
-                    $yearly_record = new YearlyRecord();
-                    $yearly_record->fiscal_year_id          = $active_fiscal_year->id;
-                    $yearly_record->yearly_record_balance   = $balance;
-                    $yearly_record->company_id              = $company_id;
-
-                    if($balance >= 0) {
-                        $yearly_record->yearly_record_status = 'dr';
-                    } else {
-                        $yearly_record->yearly_record_status = 'cr';
-                    }
+        switch ($request->input('action')) {
+            case 'save':
+                $company_id = $this->request->input('company_id');
+    
+                $rules = [
+                    'company_name'      => 'required|min:3|max:255',
+                    'company_type'      => 'required|numeric',
+                    'address'           => 'required|min:3|max:255',
+                    'owner'             => 'required|min:3|max:255',
                 
-                    $yearly_record->save();
-
+                ];
+    
+    
+    
+                $this->validate($request, $rules);
+    
+                $company_type       = $this->request->input('company_type');
+                $company_user_id    = $this->request->input('company_user_id');
+                $company_name       = $this->request->input('company_name');
+                $company_address    = $this->request->input('address');
+                $company_owner      = $this->request->input('owner');
+                $company_phone_number    = $this->request->input('phone_number');
+                // $company_vat_number      = $this->request->input('company_vat_number');
+                $subscription_start_date = $this->request->input('subscription_start_on');
+                $subscription_end_date   = $this->request->input('subscription_end_on');
+                $balance                 = $this->request->input('balance');
+    
+    
+                $status = $this->request->input('status');
+            
+    
+                $data = [
+                        'company_type_id'   => $company_type, 
+                        'company_name'      => $company_name,
+                        'company_address'   => $company_address,
+                        'company_owner'     => $company_owner,
+                        'company_phone_number' => $company_phone_number,
+                        'company_user_id'      => $company_user_id,
+                        // 'company_vat_number'   => $company_vat_number,
+                        'subscription_start_date'   => date('Y-m-d', strtotime($subscription_start_date)),
+                        'subscription_end_date'     => date('Y-m-d', strtotime($subscription_end_date)),
+                        'status'                    => $status,
+                        'updated_at'                => date('Y-m-d H:i:s')
+                    ];
+    
+    
+    
+                if($company_id == 0) {
+                    $data['created_at'] = date('Y-m-d H:i:s');
+                    $company_id = \DB::table('companies')->insertGetId($data);
+                    $message = "Record added successfully.";
+    
+                    $yearly_record = YearlyRecord::where('company_id', '=', $company_id)->get()->toArray();
+    
+                    if(count($yearly_record) == 0){
+                        $active_fiscal_year     = FiscalYear::where('current_fiscal_year', '1')->first();
+    
+    
+    
+                        $yearly_record = new YearlyRecord();
+                        $yearly_record->fiscal_year_id          = $active_fiscal_year->id;
+                        $yearly_record->yearly_record_balance   = $balance;
+                        $yearly_record->company_id              = $company_id;
+    
+                        if($balance >= 0) {
+                            $yearly_record->yearly_record_status = 'dr';
+                        } else {
+                            $yearly_record->yearly_record_status = 'cr';
+                        }
                     
-                    
+                        $yearly_record->save();
+    
+                        
+                        
+                    }
                 }
-            }
-            else {
-                \DB::table('companies')
-                ->where('id', $company_id)
-                ->update($data);
-                $message = "Record updated successfully.";
-            }
-            return redirect('/companies')->with('success','Record Added');
-        break;
+                else {
+                    \DB::table('companies')
+                    ->where('id', $company_id)
+                    ->update($data);
+                    $message = "Record updated successfully.";
+                }
+                return redirect('/companies')->with('success','Record Added');
+            break;
+    
+            case 'cancel':
+                return redirect('/companies');
+            break;
+        }
 
-        case 'cancel':
-            return redirect('/companies');
-        break;
     }
-}
-
 }
