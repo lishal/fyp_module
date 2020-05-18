bc358d4a459b73b0384e8b583fb2139d916ae93c
lishal <52682738+lishal@users.noreply.github.com>
2020-05-18 22:43:18 +0545

    Cash in hand Updated (old balance and new balance is now added and stores final cash in hand)

--- app/Http/Controllers/TrialbalanceController.php
+++ app/Http/Controllers/TrialbalanceController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Trialbalance;
+use App\Settings;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
@@ -29,12 +29,29 @@ class TrialbalanceController extends Controller
     {
         $active_fiscal_year     = FiscalYear::where('current_fiscal_year', '1')->first();
         $value=$request->input('cash_in_hand');
+        $settings= Settings::all();
+        
+        if($settings->isEmpty()){
+            $data = [
+                'settings_name' => 'Cashinhand', 
+                'settings_description' => $value,
+                'fiscal_year_id'=>$active_fiscal_year->id,
+                'updated_at' => date('Y-m-d H:i:s')
+            ];
+            \DB::table('settings')->insert($data);
+        }
+        else{
+            $cash=DB::table('settings')->where(['settings_name'=>'Cashinhand','fiscal_year_id'=>$active_fiscal_year->id])->get();
+             $cashinhand=$cash[0]->settings_description;
+             $update_cashinhand=$cashinhand+$value;
+            
+            DB::table('settings')->where(['settings_name'=>'Cashinhand','fiscal_year_id'=>$active_fiscal_year->id])->update([
 
-        DB::table('settings')->where(['settings_name'=>'Cashinhand','fiscal_year_id'=>$active_fiscal_year->id])->update([
-
-            'settings_description' => $value
-         ]);
-         //return($value);
+                'settings_description' => $update_cashinhand
+             ]);
+        }
+        
+         //return($update_cashinhand);
         return redirect('admin/trialbalance');
     }
 }
