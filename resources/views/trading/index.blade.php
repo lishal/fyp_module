c6ada1bc14a54fdfaf82422289c7b213d0dbab92
lishal <52682738+lishal@users.noreply.github.com>
2020-05-24 15:57:20 +0545

    Layout for Trading Balance

--- resources/views/trading/index.blade.php
+++ resources/views/trading/index.blade.php
@@ -3,4 +3,92 @@
 <h2>Trading Balance </h2>
 @endsection
 @section('content')
+<div>
+
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs" role="tablist">
+      <li role="presentation" class="active"><a href="#trading" aria-controls="trading" role="tab" data-toggle="tab">Trading</a></li>
+      <li role="presentation"><a href="#profitloss" aria-controls="profitloss" role="tab" data-toggle="tab">Profit Loss</a></li>
+      <li role="presentation"><a href="#balancesheet" aria-controls="blancesheet" role="tab" data-toggle="tab">Balance Sheet</a></li>
+     
+    </ul>
+
+    <!-- Tab panes -->
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="trading">
+            <!-- title row -->
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h2 class="page-header">
+                        <i class="fa fa-globe"></i> Trading Account
+                        <small class="pull-right">Date: <?php echo date('Y-m-d'); ?></small>
+                      </h2>
+                    </div>
+                    <!-- /.col -->
+                  </div>
+                  <!-- info row -->
+                  <div class="row invoice-info">
+                    
+                      <div class="form-group">
+                           <form method="post" action="#">
+                            {!! csrf_field() !!}
+                              <div class="col-md-3">
+                                  <input type="text" min="0" class="form-control" placeholder="Closing Stock" name="closing_stock" value="">
+                              </div>
+                              <div class="col-md-2">
+                                  <button type="submit" class="btn btn-default">SAVE</button>
+                              </div>
+                             </form>
+                        </div>
+                  </div>
+                  <!-- /.row -->
+
+                  <!-- Table row -->
+                  <div class="row">
+                    <div class="col-xs-6 table-responsive">
+                      <table class="table table-striped"  id="trading-debit-table">
+                        <thead>
+                          <tr>
+                          <th></th>
+                          <th><h3>DEBIT</h3></th>
+                          <th></th>
+                          
+                        </tr>
+                        <tr>
+                          <th>S.N</th>
+                          <th>Particulars</th>
+                          <th style="text-align: right;">Amount</th>
+                        
+                        </tr>
+                        </thead>
+                        <tbody>
+                           
+                        </tbody>
+                      </table>
+                    </div>
+
+                    <div class="col-xs-6 table-responsive">
+                      <table class="table table-striped" id="trading-credit-table">
+                        <thead>
+                          
+                          <th></th>
+                          <th><h3>CREDIT</h3></th>
+                          <th></th>
+                        </tr>
+                        <tr>
+                         
+                          <th>S.N</th>
+                          <th>Particulars</th>
+                          <th style="text-align: right;">Amount</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                      </table>
+                    </div>
+                    <!-- /.col -->
+                  </div>
+                  <!-- /.row -->
+        </div>
+  </div>
 @endsection
