0374f9f1070a98299a9fa3975e7c095b28557e5e
lishal <52682738+lishal@users.noreply.github.com>
2020-05-27 21:00:45 +0545

    Required fields is added in print

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Company;
+use App\FiscalYear;
 
 class PdfController extends Controller
 {
+    protected $company;
+    protected $request;
+
+    public function __construct(Company $company, Request $request)
+    {
+        $this->middleware('auth');
+        $this->company = $company;
+        $this->request = $request;
+    }
+
     public function index(Request $request)
 	{
-      
-        return view('print.index');
+        $fiscalYears            = FiscalYear::all();
+        $companies              = Company::orderBy("company_name")->get();
+        return view('print.index',['companies' => $companies, 'fiscalYears'=> $fiscalYears]);
    
 	}
 }
--- resources/views/print/index.blade.php
+++ resources/views/print/index.blade.php
@@ -3,5 +3,44 @@
 <h2>Print </h2>
 @endsection
 @section('content')
+<div class="panel-body">
+    <form class="form-horizontal" role="form" method="POST" action="{{ url('#') }}" enctype="multipart/form-data" target ="_blank">
+        {!! csrf_field() !!}
+        <div class="form-group{{ $errors->has('status') ? ' has-error' : '' }}">
+            <label class="col-md-2 control-label">Year</label>
 
+            <div class="col-md-7">
+                <select class="form-control" name="fiscalyear">
+                     @foreach ($fiscalYears as $fiscalyear)
+                        <option value="{{$fiscalyear->id}}">{{$fiscalyear->fiscal_year_name}}</option>
+                     @endforeach
+                </select>
+                @if ($errors->has('status'))
+                    <span class="help-block"><strong>{{ $errors->first('status') }}</strong></span>
+                @endif
+            </div> 
+        </div>
+        <div class="form-group{{ $errors->has('status') ? ' has-error' : '' }}">
+            <label class="col-md-2 control-label">Account</label>
+
+            <div class="col-md-7">
+                <select class="form-control" name="companies">
+                    @foreach ($companies as $company)
+                        <option value="{{$company->id}}">{{$company->company_name}}</option>
+                    @endforeach
+                    
+                </select>
+                @if ($errors->has('status'))
+                    <span class="help-block"><strong>{{ $errors->first('status') }}</strong></span>
+                @endif
+            </div> 
+        </div>
+
+        <div class="form-group bi-form-controls">
+            <div class="col-md-9 text-left">
+                <button type="submit" class="btn btn-default pull-right">Generate Pdf</button>
+            </div>
+        </div>
+    </form>
+</div>
 @endsection
